94b288383c9fbcfc513ba416e8e92ca1d451c80b
Avinash_Singh <avi13smartest@gmail.com>
2017-08-22 23:43:24 +0530

    COMMIT

---
+++ app/src/main/java/avi/singh/json_post/MainActivity.java
@@ -0,0 +1,149 @@
+package avi.singh.json_post;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import au.com.gridstone.grex.converters.GsonConverter;
+import retrofit2.Retrofit;
+import retrofit2.http.HTTP;
+
+
+
+public class MainActivity extends AppCompatActivity{
+
+    TextView content;
+    EditText fname, email;
+    String Name, Email;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialisation
+
+
+        content   =   (TextView)findViewById( R.id.status );
+        fname      =   (EditText)findViewById(R.id.fname);
+        email      =   (EditText)findViewById(R.id.email);
+
+
+
+        Button saveme=(Button)findViewById(R.id.send);
+
+        saveme.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v)
+            {
+                try{
+
+                    // CALL GetText method to make post method call
+                    GetText();
+                }
+                catch(Exception ex)
+                {
+                    content.setText(" url exeption! " );
+                }
+            }
+        });
+    }
+
+    // Create GetText Metod
+
+    public  void  GetText()  throws UnsupportedEncodingException
+    {
+        // Get user defined values
+        Name = fname.getText().toString();
+        Email   = email.getText().toString();
+
+        // Create data variable for sent values to server
+
+        String data = URLEncoder.encode("name", "UTF-8")
+                + "=" + URLEncoder.encode(Name, "UTF-8");
+
+        data += "&" + URLEncoder.encode("email", "UTF-8") + "="
+                + URLEncoder.encode(Email, "UTF-8");
+
+
+
+        String text = "";
+        BufferedReader reader=null;
+
+
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL("http://52.66.87.246/storekeyserver/requestQuote");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( data );
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+        }
+        catch(Exception ex)
+        {
+
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {}
+        }
+
+        // Show response on activity
+        content.setText( text  );
+
+    }
+
+}
+
+
+
+
+
+
